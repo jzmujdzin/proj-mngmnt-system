bced8143a709f1dcee9278904bb5f87f636a4988
jzmujdzin <jzmujdzin@gmail.com>
2022-12-13 18:21:23 +0100

    add insert_values_q for inserting values to table

--- queries/create_data.py
+++ queries/create_data.py
@@ -1,5 +1,6 @@
 from typing import Tuple
 from tools.table_data import Column
+import pandas as pd
 
 
 def create_table_q(table_name: str, column_info: Tuple[Column]) -> str:
@@ -12,3 +13,28 @@ def create_table_q(table_name: str, column_info: Tuple[Column]) -> str:
     )
                          '''
     return create_table_query
+
+
+def insert_values_q(table_name: str, df: pd.DataFrame) -> str:
+    values = ''
+    columns = ''
+    for col in df.columns.values:
+        columns += col + ','
+    columns = columns[:-1]
+    for row in df.iterrows():
+        row_values = '('
+        for i in range(len(df.columns)):
+            cell = row[1][i]
+            if isinstance(cell, int):
+                row_values += str(cell) + ','
+            else:
+                row_values += f''' '{str(cell)}','''
+        row_values = row_values[:-1]
+        row_values += '),'
+        values += row_values
+    values = values[:-1]
+    insert_values_query = f'''
+    INSERT INTO {table_name}({columns})
+    VALUES {values}
+                           '''
+    return insert_values_query
