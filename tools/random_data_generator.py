94f02e2f1d9850b7ed6bf1f106cbf04010bb1318
jzmujdzin <jzmujdzin@gmail.com>
2022-12-13 18:35:49 +0100

    add insert_data_to_table

--- tools/random_data_generator.py
+++ tools/random_data_generator.py
@@ -1,24 +1,35 @@
 import pandas as pd
-import random
 import sqlite3
 from typing import List
-from table_data import Column
+from table_data import Column, Table
+from queries.create_data import create_table_q, insert_values_q
+from loguru import logger
+from random_choice_data import get_random_choice_for_type
 
 
 class DataGenerator:
 
-    def __init__(self, table_name: str, column_info: List[Column], con: sqlite3.Connection):
+    def __init__(self, table: Table, con: sqlite3.Connection):
         """
         Generates random data for each of column in passed table_name, with provided data_ranges.
         """
-        self.table_name = table_name
-        self.column_info = column_info
+        self.table = table
         self.con = con
         self.cur = self.con.cursor()
 
-    def generate_data_for_column(self, column_name: str):
-        pass
+    def generate_data_for_column(self, col: Column) -> List:
+        return [get_random_choice_for_type(col.d_type) for row in range(self.table.rows)]
 
-    def create_table(self):
-        pass
+    def generate_data_for_table(self) -> List[List]:
+        return [self.generate_data_for_column(column) for column in self.table.column_info]
 
+    def insert_data_to_table(self) -> None:
+        df = pd.DataFrame(self.generate_data_for_table())
+        q = insert_values_q(self.table.table_name, df)
+        self.cur.execute(q)
+        logger.info(f'inserted values into {self.table.table_name}')
+
+    def create_table(self) -> None:
+        q = create_table_q(self.table.table_name, self.table.column_info)
+        self.cur.execute(q)
+        logger.info(f'created {self.table.table_name}')
