38e10d4fbd394841d5826cf56c5c51c72ab47157
jzmujdzin <jzmujdzin@gmail.com>
2022-12-14 14:10:21 +0100

    black formatting

--- tools/db_connection.py
+++ tools/db_connection.py
@@ -1,5 +1,5 @@
-import sqlite3
 import os
+import sqlite3
 from pathlib import Path
 
 
@@ -8,5 +8,5 @@ class ConnectionClient:
         self.connection = self.sqlite_con()
 
     @staticmethod
-    def sqlite_con(db_file: str = 'cpmgs_db'):
-        return sqlite3.connect(Path(os.path.abspath(os.curdir)) / '..' / db_file)
+    def sqlite_con(db_file: str = "cpmgs_db"):
+        return sqlite3.connect(Path(os.path.abspath(os.curdir)) / ".." / db_file)
